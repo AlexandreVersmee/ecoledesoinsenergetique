ac93ff22638db039e459738b76061ec2dd78cd9f
alex <versmee_alexandre@hotmail.fr>
2020-10-11 18:02:17 +0200

    Add component for dynamic Slider

---
+++ vue.js
@@ -0,0 +1,19 @@
+/*
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    Vuemode: 'hi
+    story',
+    routes: [{
+        path: '/',
+        component: require('./components/gome.vue')
+    }]
+})
+let vm = new Vue({
+    el: '#app',
+    router,
+})*/
+
